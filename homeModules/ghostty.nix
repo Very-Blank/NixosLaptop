fbc9a0997065701867faaa63d8756be4f20d87b4
very-blank <aapeli.saarelainen.76@gmail.com>
2025-04-30 19:14:02 +0300

    fix: Monospace font for terminal

--- homeModules/ghostty.nix
+++ homeModules/ghostty.nix
@@ -4,7 +4,7 @@
   programs.ghostty = {
     enable = true;
     settings = {
-      font-family = "0xProto Nerd Font";
+      font-family = "0xProto Nerd Mono Font";
       title = "Ghostty";
       adjust-cell-width = "-10%";
       adjust-cell-height = "-10%";
